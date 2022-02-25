4da61b640a703ee9ba6d5f8bac4053337eaab5d7
Tim McCloud <timmccloudtlg@users.noreply.github.com>
2022-02-25 03:53:32 +0000

    Project 2

--- project2.py
+++ project2.py
@@ -5,8 +5,15 @@ import csv
 with open("pokedex.txt", "r") as pokemon:
     for x in csv.DictReader (pokemon):
         print(f'{x["Name"]} is a {x["Type 1"]} type Pokemon.')
-        print(f'{x["Defense"]} Defense') 
-        print(f'{x["Attack"]} Attack')
+        print(f'\t{x["Defense"]} Defense') 
+        print(f'\t{x["Attack"]} Attack')
+        print(f'\t{x["Defense"]}+{x["Attack"]}')
+        # Change the sum into integers
+        # if sum =<150 print("I am a powerful Pokemon.")
+        # else print("I am a weak Pokemon.") 
+        print("\n")
+
+
 
                
 
