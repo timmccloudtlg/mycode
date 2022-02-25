ba22d93c641bde77a85daafe0362a2ad1ea9e659
Tim McCloud <timmccloudtlg@users.noreply.github.com>
2022-02-25 21:31:13 +0000

    Project1

--- project1.py
+++ project1.py
@@ -4,11 +4,11 @@
 month = input("What month were you born?")
 month.lower()
 day = int(input("What day wery you born?"))
-months = {"January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+months = ["january", "feburary", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
 
-if month in months
+if month in months:
     if month == "january":
-        sign = "Carpricorn" if (day < 20) else "Aquarius" 
+        sign = "Capricorn" if (day < 20) else "Aquarius" 
     elif month == "february":
         sign = "Aquarius" if (day < 19) else " Pices"
     elif month == "march":
@@ -22,4 +22,14 @@ if month in months
     elif month == "july":
         sign = "Cancer" if (day < 23) else "Leo"
     elif month == "august":
-        sign 
+        sign = "Leo" if (day < 23) else "Virgo"
+    elif month == "september":
+        sign = "Virgo" if (day < 23) else "Libra"
+    elif month == "october":
+        sign = "Libra" if (day < 23) else "Scorpio"
+    elif month == "november":
+        sign = "Scropio" if (day < 23) else "Sagittarius"
+    elif month == "december":
+        sign = "Sagittarius" if (day < 20) else "Capricorn"
+
+    print("Your zodiac sign is " + sign)
